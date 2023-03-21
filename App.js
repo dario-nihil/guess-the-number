a1a0e3dafed41dba3069c9048dc122bbff9d0fe8
dario-nihil <dario.nihil@gmail.com>
2023-03-21 14:09:40 +0100

    included custom fonts and installed expo splash screen package to handle splash screen on app loading

--- App.js
+++ App.js
@@ -1,15 +1,44 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { StyleSheet, ImageBackground, SafeAreaView } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
 
 import StartGameScreen from "./screens/StartGameScreen";
 import GameScreen from "./screens/GameScreen";
 import GameIsOver from "./screens/GameOverScreen";
 import Colors from "./constants/colors";
 
+SplashScreen.preventAutoHideAsync()
+  .then((res) =>
+    console.log(`SplashScreen.preventAutoHideAsync() succeded: ${res}`)
+  )
+  .catch(console.warn);
+
 export default function App() {
   const [userNumber, setUserNumber] = useState(null);
   const [gameIsOver, setGameIsover] = useState(true);
+
+  const [fontsLoaded] = useFonts({
+    "open-sans": require("./assets/fonts/OpenSans-Regular.ttf"),
+    "open-sans-bold": require("./assets/fonts/OpenSans-Bold.ttf"),
+  });
+
+  // watch for fonts to be loaded, then hide the splash screen
+  useEffect(() => {
+    const hideSplashScreen = async () => {
+      await SplashScreen.hideAsync();
+    };
+
+    if (fontsLoaded) {
+      hideSplashScreen();
+    }
+  }, [fontsLoaded]);
+
+  // Initially return null instead of Apploading />
+  if (!fontsLoaded) {
+    return null;
+  }
 
   const pickedNumberHandler = (pickedNumber) => {
     setUserNumber(pickedNumber);
